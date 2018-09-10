d52dffe245e1108e8e8e099812866c00e099bc89
sheilambadi <mbadiatieno@gmail.com>
2018-09-10 10:35:17 +0300

    Add post movie logic frontend

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,11 +1,21 @@
 var app = angular.module("myApp", []);
 app.controller("myController", function($scope, $http){
-         
-$http({
-        url: "api/movie/list_movies",
-        method: "GET"
-    }).then(function(response){
-        $scope.data=response.data;
+   
+   $scope.sendMovieData = function(){
+       var movieData = {
+           "movieName":$scope.nameMovie,
+           "movieYear":$scope.nameMovieYear,
+           "movieSynopsis":$scope.nameMovieSynopsis
+       };
+       $http.post("api/movie/create_movie", movieData);
+   };
+   
+    
+    $http({
+            url: "api/movie/list_movies",
+            method: "GET"
+        }).then(function(response){
+            $scope.data=response.data;
         });
     });
 
