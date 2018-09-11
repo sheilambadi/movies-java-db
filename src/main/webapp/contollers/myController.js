d174c341dd15e960cd7fa8db44867ea1ec501c69
sheilambadi <mbadiatieno@gmail.com>
2018-09-11 08:53:41 +0300

    Add delete angular, remove row pending

--- src/main/webapp/contollers/myController.js
+++ src/main/webapp/contollers/myController.js
@@ -1,5 +1,29 @@
 app.controller("myController", function($scope, $http, notify){
    
+   $scope.deleteMovie = function(movieId){
+       var movieData = {
+           id:movieId
+       };
+        $http({
+            url: "api/movie/delete_movie/" + movieData.id,
+            method:"DELETE"
+        }).then(function(response){
+            notify(response.data.movieName + ' Deleted!');
+            $scope.markDeleted(movieData.id);
+        }, function(response){
+            notify('Error');
+        });
+    };
+    
+    $scope.markDeleted = function(movieId){
+        angular.forEach($scope.data, function(index, movie){
+            if(movie.movieId === movieId){
+                movie.s.deleted = true;
+            }
+        });
+    };
+
+
    $scope.sendMovieData = function(){
        var movieData = {
            "movieName":$scope.nameMovie,
@@ -17,11 +41,11 @@ app.controller("myController", function($scope, $http, notify){
            notify('All fields are required!!!');
        });
    };
-   
+  
    $http({
             url: "api/movie/list_movies",
             method: "GET"
         }).then(function(response){
             $scope.data=response.data;
         });
-    });
+    }); 
